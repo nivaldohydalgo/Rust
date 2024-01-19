96af97b444d3816fed6c890b155db5a5bd54a51f
nivaldohydalgo <32170309+nivaldohydalgo@users.noreply.github.com>
2024-01-19 19:27:05 -0300

    Delete hello-world/target/debug directory

--- hello-world/target/debug/deps/hello_world-2a46f444aa3b9b1a.d
+++
@@ -1,5 +0,0 @@
-/home/nivaldo/Developer/Rust/hello-world/target/debug/deps/hello_world-2a46f444aa3b9b1a: src/main.rs
-
-/home/nivaldo/Developer/Rust/hello-world/target/debug/deps/hello_world-2a46f444aa3b9b1a.d: src/main.rs
-
-src/main.rs:
